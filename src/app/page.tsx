398d5fb76cb7e3e1fdf334e29ef3e2bca1a4216d
Jace <contactjacenair@gmail.com>
2025-01-13 14:44:36 +0000

    Add Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@/sections/Header";
+import { Hero } from "@/sections/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 }
